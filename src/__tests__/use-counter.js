d226b32454c7ab79b57b62384f7dcd39a6dca31d
Sung M. Kim <sungmkim80@gmail.com>
2020-01-01 14:41:32 -0500

    Learned how to use 'renderHook'

--- src/__tests__/use-counter.js
+++ src/__tests__/use-counter.js
@@ -1,20 +1,8 @@
-import React from 'react'
-
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react-hooks'
 import {useCounter} from '../use-counter'
 
-function setup({initialProps} = {}) {
-  const result = {}
-  const TestComponent = props => {
-    result.current = useCounter(props)
-    return null
-  }
-  render(<TestComponent {...initialProps} />)
-  return result
-}
-
 test('expose the count and increment/decrement functions', () => {
-  const result = setup()
+  const {result} = renderHook(useCounter)
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
   expect(result.current.count).toBe(1)
@@ -23,12 +11,12 @@ test('expose the count and increment/decrement functions', () => {
 })
 
 test('allow customization of the initial count', () => {
-  const result = setup({initialProps: {initialCount: 3}})
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 3}})
   expect(result.current.count).toBe(3)
 })
 
 test('allow customoization of the step', () => {
-  const result = setup({initialProps: {step: 2}})
+  const {result} = renderHook(useCounter, {initialProps: {step: 2}})
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
   expect(result.current.count).toBe(2)
