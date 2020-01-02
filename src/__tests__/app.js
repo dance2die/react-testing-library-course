7a3b1defb51a19474e6d92da4ed5188832f18e70
Sung M. Kim <sungmkim80@gmail.com>
2020-01-01 20:08:57 -0500

    changed fireEvent to use event

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import user from '@testing-library/user-event'
+import {render} from '@testing-library/react'
 
 import {submitForm as mockSubmitForm} from '../api'
-import App from '../app'
+// import App from '../app'
+import App from '../app-reach-router'
 
 jest.mock('../api')
 
@@ -11,24 +13,20 @@ test('Can fill out a form across multiple pages', async () => {
   const testData = {food: 'test food', drink: 'test drink'}
   const {findByLabelText, findByText} = render(<App />)
 
-  fireEvent.click(await findByText(/fill.*form/i))
+  user.click(await findByText(/fill.*form/i))
 
-  fireEvent.change(await findByLabelText(/food/i), {
-    target: {value: testData.food},
-  })
-  fireEvent.click(await findByText(/next/i))
+  user.type(await findByLabelText(/food/i), testData.food)
+  user.click(await findByText(/next/i))
 
-  fireEvent.change(await findByLabelText(/drink/i), {
-    target: {value: testData.drink},
-  })
-  fireEvent.click(await findByText(/review/i))
+  user.type(await findByLabelText(/drink/i), testData.drink)
+  user.click(await findByText(/review/i))
 
   expect(await findByLabelText(/food/i)).toHaveTextContent(testData.food)
   expect(await findByLabelText(/drink/i)).toHaveTextContent(testData.drink)
 
-  fireEvent.click(await findByText(/confirm/i, {selector: 'button'}))
+  user.click(await findByText(/confirm/i, {selector: 'button'}))
 
-  fireEvent.click(await findByText(/home/i))
+  user.click(await findByText(/home/i))
 
   expect(await findByText(/welcome home/i)).toBeInTheDocument()
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
